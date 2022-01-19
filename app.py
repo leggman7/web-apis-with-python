74b39b27283285eca0b6b148a55ba0ca9a358c0f
leggman7 <97209597+leggman7@users.noreply.github.com>
2022-01-19 12:51:33 -0800

    Update app.py
    finished the chapter, made it slightly differently

--- app.py
+++ app.py
@@ -14,4 +14,23 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+
+    fname = request.args.get('fname')
+    lname = request.args.get('lname')
+
+    if not fname and not lname:
+        return jsonify({'status': 'error'})
+    elif fname and not lname:
+        response = {'data':f':hello {fname}'}
+    elif lname and not fname:
+        response = {'data': f'good day, Mr. {lname}'}
+    elif lname and fname:
+        response = {'data':f'hey there {fname} {lname}'}
+
+
+    #response = {'data':f'Hello, {name}'} #for the previous version that didn't have f and l name
+    return jsonify(response)
+
+@app.get("/ryan")
+def index2():
+    return "Ryan's Awesome"
